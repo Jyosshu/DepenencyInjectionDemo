7188f16d07bd2a836e58c1cbcf25f041970894b3
Josh Wygle <jwygle@gmail.com>
2020-09-18 18:15:59 -0400

    Added gitignore as well as the existing code.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,24 @@
+﻿using Autofac;
+using DemoLibrary;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // https://autofac.org/
+
+            var container = ContainerConfig.Configure();
+
+            using (var scope = container.BeginLifetimeScope())
+            {
+                var app = scope.Resolve<IApplication>();
+                app.Run();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
